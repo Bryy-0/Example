ffa6dc24d8cf12f5081a6aab07496fc7f01491c3
Bryy <brylle11062007@gmail.com>
2026-04-20 11:55:13 +0800

    Leaderboard - html

--- redirect.js
+++ redirect.js
@@ -1,6 +1,6 @@
         // Redirect logic: waits 3 seconds then goes home
         window.onload = function() {
             setTimeout(() => {
-                window.location.href ='home.php'; 
+                window.location.href ='home.html'; 
             }, 3000); // 3000ms = 3 seconds
         };
